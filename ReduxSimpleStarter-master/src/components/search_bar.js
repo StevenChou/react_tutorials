6545111e32ac8f91f56bac91c54d3cd0968059dd
Unknown <steven.chou.second@gmail.com>
2017-06-24 14:39:07 +0800

    Class-Based Components

--- ReduxSimpleStarter-master/src/components/search_bar.js
+++ ReduxSimpleStarter-master/src/components/search_bar.js
@@ -1,7 +1,15 @@
-import React from 'react'; // translate the JSX to normal Javascript
+import React, { Component } from 'react'; // translate the JSX to normal Javascript
 
-const SearchBar = () => {
-  return <input />;  // React.createElement
+// const SearchBar = () => {
+//   return <input />;  // React.createElement
+// }
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <input value="hi!!"/>
+    );
+  }
 }
 
 export default SearchBar;
