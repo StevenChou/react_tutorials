35ae5b89f31ba9c28ea62744f48f817181a32e5d
Unknown <steven.chou.second@gmail.com>
2017-06-24 12:36:00 +0800

    Render Targets

--- ReduxSimpleStarter-master/src/index.js
+++ ReduxSimpleStarter-master/src/index.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from 'react';  // 建立或管理 component
+import ReactDOM from 'react-dom'; // 與真實 DOM 的互動
 
-import App from './components/app';
-import reducers from './reducers';
+// Create a new component. This component should produce 
+// some HTML
+// const App = function() {
+//   return <div>Hi!</div>;
+// }
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// const App = () => {
+//   return <div>Hi</div>;
+// }
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const App = () => <div>Hi!</div>
+
+// const is ready only
+// App = 5;
+
+
+
+// Take this component's generated HTML and put it 
+// on the page(in the DOM)
+
+// ** App  ===> This is a class of component 
+//<App /> is a instance
+ReactDOM.render(<App />, document.querySelector('.container'));
