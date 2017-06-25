ce4083ea775563bc0feb027149c7cd0aa203ecda
Unknown <steven.chou.second@gmail.com>
2017-06-25 15:13:44 +0800

    Video List Items

--- ReduxSimpleStarter-master/src/components/video_list_item.js
+++ ReduxSimpleStarter-master/src/components/video_list_item.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
-  return <li>video</li>
+// const VideoListItem = (props) => {
+//   const video = props.video;
+
+//   return <li>video</li>
+// };
+
+const VideoListItem = ({video}) => {
+  const imagUrl = video.snippet.thumbnails.default.url;
+
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={imagUrl} />
+        </div>
+
+        <div className="media-body">
+          <div className="media-heading">{video.snippet.title}</div>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default VideoListItem;
