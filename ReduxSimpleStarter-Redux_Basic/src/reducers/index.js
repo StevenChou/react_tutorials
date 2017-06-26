0e35bd20433e0a84d026295b59eb9477cf728381
Unknown <steven.chou.second@gmail.com>
2017-06-26 14:59:15 +0800

    Containers - Connecting Redux to React

--- ReduxSimpleStarter-Redux_Basic/src/reducers/index.js
+++ ReduxSimpleStarter-Redux_Basic/src/reducers/index.js
@@ -3,6 +3,7 @@ import { combineReducers } from 'redux';
 import BooksReducer from './reducer_books';
 
 // mapping of our state right here
+// redux state
 const rootReducer = combineReducers({
   books: BooksReducer
 });
