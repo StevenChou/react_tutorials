171ac524d9acde69f9d87914affe0a585e7d3882
Unknown <steven.chou.second@gmail.com>
2017-06-28 16:31:59 +0800

    Form Elements in React

--- ReduxSimpleStarter-Redux_intermediate/src/containers/search_bar.js
+++ ReduxSimpleStarter-Redux_intermediate/src/containers/search_bar.js
@@ -9,14 +9,20 @@ export default class SearchBar extends Component {
     this.onInputChange = this.onInputChange.bind(this);
   }
 
-  onInputChange(event){
+  onInputChange(event) {
     // console.log(event.target.value);
     this.setState({ term: event.target.value });
   }
 
+  onFormSubmit(event) {
+    event.preventDefault();
+
+    // We need to go and fetch weather data
+  }
+
   render() {
     return (
-      <form className="input-group">
+      <form onSubmit={this.onFormSubmit} className="input-group">
         <input
           placeholder="Get a five-day forecast in your favorite cities."
           className="form-control"
