506f9bc5facdb841c7209902df2fe08e5ef02f91
Unknown <steven.chou.second@gmail.com>
2017-06-29 21:26:57 +0800

    Ajax Requests with Axios

--- ReduxSimpleStarter-Redux_intermediate/src/actions/index.js
+++ ReduxSimpleStarter-Redux_intermediate/src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = '7f4ad5211143210b16eb098686021bb7';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: 'FETCH_WEATHER',
+    payload: request
+  }
+};
