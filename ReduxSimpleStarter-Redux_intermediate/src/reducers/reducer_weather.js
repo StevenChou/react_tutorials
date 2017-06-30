fbaa8a29c581360b8fa8ea22ed26ab5333a5096c
Unknown <steven.chou.second@gmail.com>
2017-06-30 14:58:06 +0800

    Adding Sparkline Charts

--- ReduxSimpleStarter-Redux_intermediate/src/reducers/reducer_weather.js
+++ ReduxSimpleStarter-Redux_intermediate/src/reducers/reducer_weather.js
@@ -1,8 +1,10 @@
 import { FETCH_WEATHER } from './../actions/index';
 
 export default function(state = [], action) {
+  
   switch(action.type) {
     case FETCH_WEATHER:
+      console.log('disp:', action.payload.data);
       // return state.push(action.payload.data);
 
       // ** return new instance of state ** ES5
