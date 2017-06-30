b347e591de6a9f98f5c84d22ec83df74af387fdd
Unknown <steven.chou.second@gmail.com>
2017-06-30 12:11:32 +0800

    ** Avoiding State Mutations in Reducers **
    永遠回傳新的 state 物件

--- ReduxSimpleStarter-Redux_intermediate/src/reducers/reducer_weather.js
+++ ReduxSimpleStarter-Redux_intermediate/src/reducers/reducer_weather.js
@@ -1,5 +1,15 @@
-export default function(state = null, action) {
-  console.log('Action received', action);
+import { FETCH_WEATHER } from './../actions/index';
 
+export default function(state = [], action) {
+  switch(action.type) {
+    case FETCH_WEATHER:
+      // return state.push(action.payload.data);
+
+      // ** return new instance of state ** ES5
+      // return state.concat([action.payload.data]);
+
+      // ** return new instance of state ** ES6
+      return [action.payload.data, ...state];
+  }
   return state;
 };
