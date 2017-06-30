c99e46e6e744202500ae5ad2251f1cfd9f334d23
Unknown <steven.chou.second@gmail.com>
2017-06-30 17:22:19 +0800

    Labeling of Units

--- ReduxSimpleStarter-Redux_intermediate/src/containers/weather_list.js
+++ ReduxSimpleStarter-Redux_intermediate/src/containers/weather_list.js
@@ -1,33 +1,42 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import Char from './../components/char';
+import Chart from './../components/chart';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
     const temps = cityData.list.map((weather) => weather.main.temp);
     // console.log('temps', temps);
+    const pressures = cityData.list.map((weather) => weather.main.pressure);
+    const humidities = cityData.list.map((weather) => weather.main.humidity);
 
     return (
       <tr key={name}>
         <td>{name}</td>
         <td>
-          <Char data={temps} color="orange" />
+          <Chart data={temps} color="orange" units="K" />
+        </td>
+        <td>
+          <Chart data={pressures} color="green" units="hPa" />
+        </td>
+        <td>
+          <Chart data={humidities} color="black" units="%" />
         </td>
       </tr>
     );
   }
 
   render() {
+    const tdStyle = {width: '25%'};
     return (
       <table className="table table-hover">
         <thead>
           <tr>
-            <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th style={tdStyle}>City</th>
+            <th style={tdStyle}>Temperature (K)</th>
+            <th style={tdStyle}>Pressure (hPa)</th>
+            <th style={tdStyle}>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
