eb1f2e40d25491fce969fadd72d04452651461d6
Unknown <steven.chou.second@gmail.com>
2017-07-03 22:12:38 +0800

    The Search Field - A Controlled Component

--- reactnd-contacts-complete-starter-files-added/src/components/ListContacts.js
+++ reactnd-contacts-complete-starter-files-added/src/components/ListContacts.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const ListContacts = (props) => (  
-  <ol className="contact-list">
-        {props.contacts.map(contact => (
-          <li key={contact.id} className="contact-list-item">
-            <div className="contact-avatar" style={{
-              backgroundImage: `url(${contact.avatarURL})`
-            }} />
-            <div className="contact-details">
-              <p>{contact.name}</p>
-              <p>{contact.email}</p>
-            </div>
-            <button onClick={() => props.onDeleteContract(contact)} className="contact-remove">
-              Remove
-            </button>
-          </li>
-        ))}
-      </ol>
-);
+// const ListContacts = (props) => (  
+//   <ol className="contact-list">
+//         {props.contacts.map(contact => (
+//           <li key={contact.id} className="contact-list-item">
+//             <div className="contact-avatar" style={{
+//               backgroundImage: `url(${contact.avatarURL})`
+//             }} />
+//             <div className="contact-details">
+//               <p>{contact.name}</p>
+//               <p>{contact.email}</p>
+//             </div>
+//             <button onClick={() => props.onDeleteContract(contact)} className="contact-remove">
+//               Remove
+//             </button>
+//           </li>
+//         ))}
+//       </ol>
+// );
 
 // function ListContacts(props) {
 //   return (
@@ -41,32 +41,55 @@ const ListContacts = (props) => (
 //     ); 
 // }
 
-// class ListContacts extends Component {
-//   render() {
-//     return (
-//       <ol className="contact-list">
-//         {this.props.contacts.map(contact => (
-//           <li key={contact.id} className="contact-list-item">
-//             <div className="contact-avatar" style={{
-//               backgroundImage: `url(${contact.avatarURL})`
-//             }} />
-//             <div className="contact-details">
-//               <p>{contact.name}</p>
-//               <p>{contact.email}</p>
-//             </div>
-//             <button className="contact-remove">
-//               Remove
-//             </button>
-//           </li>
-//         ))}
-//       </ol>
-//     );
-//   }
-// }
+class ListContacts extends Component {
+  static propTypes = {
+    contacts: PropTypes.array.isRequired,
+    onDeleteContract: PropTypes.func.isRequired
+  }
 
-ListContacts.propTypes = {
-  contacts: PropTypes.array.isRequired,
-  onDeleteContract: PropTypes.func.isRequired
-};
+  state = { 
+    query: ''
+  }
+
+  updateQuery = (query) => {
+    this.setState({ query: query.trim() });
+  }
+
+  render() {
+    return (
+      <div className="list-contacts">
+        <div className="list-contacts-top">
+          <input
+            className="search-contacts"
+            type="text"
+            placeholder="Search contacts"
+            value={this.state.query}
+            onChange={(event) => this.updateQuery(event.target.value)} />
+        </div>
+        <ol className="contact-list">
+        {this.props.contacts.map(contact => (
+          <li key={contact.id} className="contact-list-item">
+            <div className="contact-avatar" style={{
+              backgroundImage: `url(${contact.avatarURL})`
+            }} />
+            <div className="contact-details">
+              <p>{contact.name}</p>
+              <p>{contact.email}</p>
+            </div>
+            <button onClick={() => this.props.onDeleteContract(contact)} className="contact-remove">
+              Remove
+            </button>
+          </li>
+        ))}
+      </ol>
+      </div>      
+    );
+  }
+}
+
+// ListContacts.propTypes = {
+//   contacts: PropTypes.array.isRequired,
+//   onDeleteContract: PropTypes.func.isRequired
+// };
 
 export default ListContacts;
