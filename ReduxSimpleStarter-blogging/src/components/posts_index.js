8a82e10d319f7789a805515eba6d0dd7977670c7
Unknown <steven.chou.second@gmail.com>
2017-07-28 18:49:27 +0800

    Rendering a List of Posts

--- ReduxSimpleStarter-blogging/src/components/posts_index.js
+++ ReduxSimpleStarter-blogging/src/components/posts_index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import _ from 'lodash'
 
 import { fetchPosts } from './../actions'
 
@@ -8,15 +9,33 @@ class PostsIndex extends Component {
     this.props.fetchPosts()
   }
 
+  renderPost() {
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
+      )
+    })
+  }
+
   render() {
+    console.log(this.props.posts)
     return (
       <div>
-        Posts Index
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPost()}
+        </ul>
       </div>
     )
   }
 }
 
+function mapStateToProps(state) {
+  return { posts: state.posts }
+}
+
 // 第一個參數 state
 // export default connect(null, { fetchPosts: fetchPosts })(PostsIndex)
-export default connect(null, { fetchPosts })(PostsIndex)
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex)
