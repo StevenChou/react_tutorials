dc9c4258cebf02d70cd62f4c569e0c4be0b3dbfb
Unknown <steven.chou.second@gmail.com>
2017-07-30 14:42:49 +0800

    The Field Component

--- ReduxSimpleStarter-blogging/src/components/posts_new.js
+++ ReduxSimpleStarter-blogging/src/components/posts_new.js
@@ -2,10 +2,37 @@ import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form'
 
 class PostsNew extends Component {
+  // field argument 讓 Field component 相應輸入的值的變化
+  renderTitleField(field) {
+    // return (
+    //   <div>
+    //     <input
+    //       onChange={field.input.onChange}
+    //       onFocus={field.input.onFocus}
+    //       onBlur={field.input.onBlur}
+    //       .....
+    //     />
+    //   </div>
+    // )
+
+    // 一次把所有 field.input 的 event handlers 帶出
+    return (
+      <div>
+        <input
+          type
+          {...field.input}
+        />
+      </div>
+    )
+  }
+
   render() {
     return (
       <form>
-
+        <Field
+          name="title"
+          component={this.renderTitleField}
+        />
       </form>
     )
   }
