40f63dc0c04840c9ca6ee6840151154aebc8e468
Unknown <steven.chou.second@gmail.com>
2017-07-30 14:00:24 +0800

    Setting Up Redux Form

--- ReduxSimpleStarter-blogging/src/components/posts_new.js
+++ ReduxSimpleStarter-blogging/src/components/posts_new.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
+import { Field, reduxForm } from 'redux-form'
 
 class PostsNew extends Component {
   render() {
     return (
-      <div>
-        PostsNew!
-      </div>
+      <form>
+
+      </form>
     )
   }
 }
 
-export default PostsNew
+export default reduxForm({
+  form: 'PostsNewForm'  // name of the form
+})(PostsNew)
+
+// *** 可以 merge 不同 js 中，相同 form name 的 state
+// PostEdit.js
+// export default reduxForm({
+//   form: 'PostsNewForm'  // name of the form
+// })(PostsNew)
