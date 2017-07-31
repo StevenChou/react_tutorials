c9f648d402864c6e614fe27cc877eb5710caf3a0
Unknown <steven.chou.second@gmail.com>
2017-07-31 13:29:00 +0800

    Receiving New Posts

--- ReduxSimpleStarter-blogging/src/actions/index.js
+++ ReduxSimpleStarter-blogging/src/actions/index.js
@@ -2,13 +2,14 @@ import axios from 'axios'
 
 export const FETCH_POSTS = 'fetch_posts'
 export const CREATE_POST = 'create_post'
+export const FETCH_POST = 'fetch_post'
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api'
 const API_KEY = '?key=stevenchou1224'
 
 // action creator
 export function fetchPosts() {
-  const request = axios.request(`${ROOT_URL}/posts${API_KEY}`)
+  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`)
 
   return {
     type: FETCH_POSTS,
@@ -23,6 +24,16 @@ export function createPost(values, callback) {
 
   return {
     type: CREATE_POST,
+    payload: request
+  }
+}
+
+// action creator
+export function fetchPost(id) {
+  const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`)
+
+  return {
+    type: FETCH_POST,
     payload: request
   }
 }
--- ReduxSimpleStarter-blogging/src/reducers/reducer_posts.js
+++ ReduxSimpleStarter-blogging/src/reducers/reducer_posts.js
@@ -1,11 +1,20 @@
 import _ from 'lodash'
 
-import { FETCH_POSTS } from './../actions'
+import { FETCH_POSTS, FETCH_POST } from './../actions'
 
 // receive previous state
 // 設定 state 初始值
 export default function(state = {}, action) {
   switch (action.type) {
+    case FETCH_POST:
+      // ES5
+      // const post = action.payload.data
+      // const newState = { ...state }
+      // newState[post.id] = post
+      // return newState
+
+      // ES6
+      return { ...state, [action.payload.data.id]: action.payload.data}
     case FETCH_POSTS:
       // 經過 redux-promise middleware 處理，可以直接取得資料
       // console.log(action.payload.data) // [post1, post2]
