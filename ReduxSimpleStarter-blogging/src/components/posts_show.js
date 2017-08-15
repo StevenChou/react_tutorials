33ecbfd7faec9375385b01d8a30d91bf68e5e7e5
Unknown <steven.chou.second@gmail.com>
2017-08-15 11:29:44 +0800

    Selecting from OwnProps

--- ReduxSimpleStarter-blogging/src/components/posts_show.js
+++ ReduxSimpleStarter-blogging/src/components/posts_show.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import { fetchPost } from './../actions'
 
 class PostsShow extends Component {
+  componentDidMount() {
+    const { id } = this.props.match.params
+    this.props.fetchPost(id)
+  }
+
   render() {
+    // this.props === ownProps
     return (
       <div>
         Post Show!
@@ -10,4 +19,8 @@ class PostsShow extends Component {
   }
 }
 
-export default PostsShow
+function mapStateToProps({ posts }, ownProps) {
+  return { post: posts[ownProps.match.params.id] }
+}
+
+export default connect(nmapStateToProps, { fetchPost })(PostsShow)
