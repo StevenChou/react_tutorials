1b03d9a792b05fb2581549a2b4e82227606f9ee7
Unknown <steven.chou.second@gmail.com>
2017-08-15 12:12:23 +0800

    ** Data Dependencies

--- ReduxSimpleStarter-blogging/src/components/posts_show.js
+++ ReduxSimpleStarter-blogging/src/components/posts_show.js
@@ -6,21 +6,33 @@ import { fetchPost } from './../actions'
 class PostsShow extends Component {
   componentDidMount() {
     const { id } = this.props.match.params
+    console.log('[trace2] ownProps.match.params.id=', id)
     this.props.fetchPost(id)
   }
 
   render() {
     // this.props === ownProps
+    const { post } = this.props
+
+    // *** 當 component 第一次 render 時，post is undefined!!
+    if(!post) {
+      return <div>Loading...</div>
+    }
     return (
       <div>
-        Post Show!
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
     )
   }
 }
 
 function mapStateToProps({ posts }, ownProps) {
+  console.log('[trace1] ownProps.match.params.id=', ownProps.match.params.id)
+  console.log('[trace3] posts[ownProps.match.params.id]=', posts[ownProps.match.params.id])
+  console.log('[trace4] posts', posts)
   return { post: posts[ownProps.match.params.id] }
 }
 
-export default connect(nmapStateToProps, { fetchPost })(PostsShow)
+export default connect(mapStateToProps, { fetchPost })(PostsShow)
