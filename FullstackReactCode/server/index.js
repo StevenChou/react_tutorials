68d5acfdff37cf69bf8e7e6e4960d53a5f54ddb3
Unknown <steven.chou.second@gmail.com>
2017-08-17 18:27:20 +0800

    Generating Express Apps

---
+++ FullstackReactCode/server/index.js
@@ -0,0 +1,9 @@
+// Node js support common js modules ==> require
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+app.listen(5000);
