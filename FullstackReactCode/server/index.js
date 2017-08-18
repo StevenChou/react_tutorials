a9862d3b47f7ab17a6ac7b928b085c493000f78c
Unknown <steven.chou.second@gmail.com>
2017-08-18 14:28:41 +0800

    Verifying Heroku Deployment

--- FullstackReactCode/server/index.js
+++ FullstackReactCode/server/index.js
@@ -6,4 +6,7 @@ app.get('/', (req, res) => {
   res.send({ hi: 'there' });
 });
 
-app.listen(5000);
+// HEROKU DEPLOYMENT Step 1
+// 為了支援 HEROKU 動態產生的 PORT (5000 是在開發環境使用)
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
