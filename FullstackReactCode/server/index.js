5e6f12227fa916876f325b87ac7b59882becdb47
Unknown <steven.chou.second@gmail.com>
2017-08-18 18:14:58 +0800

    Passport Setup

--- FullstackReactCode/server/index.js
+++ FullstackReactCode/server/index.js
@@ -1,10 +1,15 @@
 // Node js support common js modules ==> require
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+// generic register
+// passport i want you to be aware that there is a new strategy available and here it is
+// make use of it
+passport.use(new GoogleStrategy())
+
 
 // HEROKU DEPLOYMENT Step 1
 // 為了支援 HEROKU 動態產生的 PORT (5000 是在開發環境使用)
