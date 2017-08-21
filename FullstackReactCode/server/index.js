7b55e23965b89dd235170cc676bb754895e0159a
Unknown <steven.chou.second@gmail.com>
2017-08-21 16:55:06 +0800

    Google Strategy Options

--- FullstackReactCode/server/index.js
+++ FullstackReactCode/server/index.js
@@ -2,14 +2,25 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
 // generic register
 // passport i want you to be aware that there is a new strategy available and here it is
 // make use of it
-passport.use(new GoogleStrategy())
-
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
 
 // HEROKU DEPLOYMENT Step 1
 // 為了支援 HEROKU 動態產生的 PORT (5000 是在開發環境使用)
